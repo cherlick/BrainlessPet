39adce7221bc2ece39d147036af56991c20ddf49
Jose Ferreira <cherlick@msn.com>
2022-09-24 23:43:21 +0200

    Add Scenechange system

---
+++ Assets/Scripts/SceneChangeSystem/SceneNamesEnum.cs
@@ -0,0 +1,11 @@
+namespace BrainlessPet.ScenesChangeSystem
+{
+	public enum SceneNamesEnum
+	{
+		Core,
+		MainMenu,
+		Level_1,
+		Level_2,
+		Level_3,
+	}
+}
---
+++ Assets/Scripts/SceneChangeSystem/ScenesManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BrainlessPet.ScenesChangeSystem
+{
+    public static class ScenesManager {
+        public static void LoadScene(int index, LoadSceneMode mode = LoadSceneMode.Single)
+        {
+            if(index < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(index, mode);
+            }
+        }
+
+        public static void LoadScene(SceneNamesEnum sceneName, bool isAdditive = false)
+        {
+            if (IsValidScene(sceneName.ToString()))
+            {
+                SceneManager.LoadScene(sceneName.ToString(), isAdditive ? LoadSceneMode.Additive : LoadSceneMode.Single);
+            }
+        }
+
+        public static void UnloadScene(SceneNamesEnum sceneName)
+        {
+            try
+            {
+                SceneManager.UnloadSceneAsync(sceneName.ToString());
+            }
+            catch (System.Exception)
+            {
+                Debug.LogWarning($"Unable to unload scene {sceneName}");
+            }
+        }
+
+        public static void RestartScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public static int GetNumberOfActiveScenes() => SceneManager.sceneCount;
+
+        public static int GetLatestSceneAdded()
+        {
+            var scene = SceneManager.GetSceneAt(GetNumberOfActiveScenes()-1);
+            return scene.buildIndex;
+        }
+
+        public static bool IsValidScene(string sceneName)
+        {
+            int sceneIndex = SceneUtility.GetBuildIndexByScenePath(sceneName);
+
+            return sceneIndex >= 0 ? true : false;
+        }
+
+        
+    }
+}
